701a4b9750b8cab60227e02009e2000fbdaac00f
Arnav Gupta <championswimmer@gmail.com>
2014-09-05 04:53:07 +0530

    add read methods

--- libHCE/src/main/java/edu/dce/nfc/libhce/ReaderMode.java
+++ libHCE/src/main/java/edu/dce/nfc/libhce/ReaderMode.java
@@ -1,7 +1,32 @@
 package edu.dce.nfc.libhce;
 
+import android.app.Activity;
+import android.nfc.NfcAdapter;
+import android.util.Log;
+
+import edu.dce.nfc.libhce.reader.CardReader;
+
 /**
  * Created by championswimmer on 5/9/14.
  */
 public class ReaderMode {
+
+    public static final String TAG = "libHCE-ReaderMode";
+    public static int READER_FLAGS =
+            NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
+
+    private CardReader mCardReader;
+
+    public ReaderMode(CardReader.ReadCallBack ac) {
+        mCardReader = new CardReader(ac);
+    }
+
+    public void enableReaderMode(Activity act) {
+        Log.i(TAG, "Enabling reader mode");
+
+        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(act);
+        if (nfc != null) {
+            nfc.enableReaderMode(act, mCardReader, READER_FLAGS, null);
+        }
+    }
 }
