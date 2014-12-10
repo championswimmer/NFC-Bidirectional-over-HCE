3086f6a0d8e62585c637b614b0c5043fb50388a3
Arnav Gupta <championswimmer@gmail.com>
2014-12-11 03:55:56 +0530

    update documentation
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ libHCE-documentation/assets/navtree_data.js
@@ -0,0 +1,14 @@
+var NAVTREE_DATA =
+[ [ "edu.dce.nfc.libhce", "edu/dce/nfc/libhce/package-summary.html", [ [ "Classes", null, [ [ "EmulatorService", "edu/dce/nfc/libhce/EmulatorService.html", null, "" ], [ "ReaderActivity", "edu/dce/nfc/libhce/ReaderActivity.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "edu.dce.nfc.libhce.common", "edu/dce/nfc/libhce/common/package-summary.html", [ [ "Classes", null, [ [ "Headers", "edu/dce/nfc/libhce/common/Headers.html", null, "" ], [ "Utils", "edu/dce/nfc/libhce/common/Utils.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "edu.dce.nfc.libhce.emulator", "edu/dce/nfc/libhce/emulator/package-summary.html", [ [ "Classes", null, [ [ "CardEmulationWrapperService", "edu/dce/nfc/libhce/emulator/CardEmulationWrapperService.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "edu.dce.nfc.libhce.reader", "edu/dce/nfc/libhce/reader/package-summary.html", [ [ "Interfaces", null, [ [ "CardReader.ReadCallBack", "edu/dce/nfc/libhce/reader/CardReader.ReadCallBack.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "CardReader", "edu/dce/nfc/libhce/reader/CardReader.html", null, "" ], [ "TransceiveResult", "edu/dce/nfc/libhce/reader/TransceiveResult.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ libHCE-documentation/reference/lists.js
@@ -0,0 +1,15 @@
+var DATA = [
+      { id:0, label:"edu.dce.nfc.libhce", link:"edu/dce/nfc/libhce/package-summary.html", type:"package" },
+      { id:1, label:"edu.dce.nfc.libhce.EmulatorService", link:"edu/dce/nfc/libhce/EmulatorService.html", type:"class" },
+      { id:2, label:"edu.dce.nfc.libhce.ReaderActivity", link:"edu/dce/nfc/libhce/ReaderActivity.html", type:"class" },
+      { id:3, label:"edu.dce.nfc.libhce.common", link:"edu/dce/nfc/libhce/common/package-summary.html", type:"package" },
+      { id:4, label:"edu.dce.nfc.libhce.common.Headers", link:"edu/dce/nfc/libhce/common/Headers.html", type:"class" },
+      { id:5, label:"edu.dce.nfc.libhce.common.Utils", link:"edu/dce/nfc/libhce/common/Utils.html", type:"class" },
+      { id:6, label:"edu.dce.nfc.libhce.emulator", link:"edu/dce/nfc/libhce/emulator/package-summary.html", type:"package" },
+      { id:7, label:"edu.dce.nfc.libhce.emulator.CardEmulationWrapperService", link:"edu/dce/nfc/libhce/emulator/CardEmulationWrapperService.html", type:"class" },
+      { id:8, label:"edu.dce.nfc.libhce.reader", link:"edu/dce/nfc/libhce/reader/package-summary.html", type:"package" },
+      { id:9, label:"edu.dce.nfc.libhce.reader.CardReader", link:"edu/dce/nfc/libhce/reader/CardReader.html", type:"class" },
+      { id:10, label:"edu.dce.nfc.libhce.reader.CardReader.ReadCallBack", link:"edu/dce/nfc/libhce/reader/CardReader.ReadCallBack.html", type:"class" },
+      { id:11, label:"edu.dce.nfc.libhce.reader.TransceiveResult", link:"edu/dce/nfc/libhce/reader/TransceiveResult.html", type:"class" }
+
+    ];
